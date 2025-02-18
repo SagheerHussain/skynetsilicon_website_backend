bf92585d88767e2c0b9a189fc0d4e88a29345009
Muhammad Shayan <shayanarif666@gmail.com>
2025-02-18 11:23:39 +0500

    Path Change

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use('/uploads', express.static('public/Images'));
+app.use("/uploads", express.static(path.join(__dirname, "public/Images")));
 
 // Routing
 app.use("/api/portfolio", portfolioRoutes);
