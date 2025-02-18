cd236ec96b95361ba4ccefac8c4a6b25e28b743d
Muhammad Shayan <shayanarif666@gmail.com>
2025-02-18 11:34:55 +0500

    Changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const axios = require("axios");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -28,7 +29,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use('/uploads', express.static('public/Images'));
+app.use("/uploads", express.static(path.join(__dirname, "public/Images")));
 
 // Routing
 app.use("/api/portfolio", portfolioRoutes);
