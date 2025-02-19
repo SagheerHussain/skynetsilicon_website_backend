f25b62be482023c112e75552537eb5a5b4562894
Muhammad Shayan <shayanarif666@gmail.com>
2025-02-19 11:04:59 +0500

    Changes

--- server.js
+++ server.js
@@ -18,9 +18,19 @@ const categoryRoutes = require("./Routes/categoryRoutes");
 
 // Middlewares
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    const allowedOrigins = ["http://localhost:5173", "https://skynetsilicon.com"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200); // Handle preflight requests
+    }
+
     next();
 });
 app.use(express.json({ limit: "50mb" }));
