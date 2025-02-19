0b7394c53a255037c093bbda5a195b8c10178ebc
Muhammad Shayan <shayanarif666@gmail.com>
2025-02-19 10:59:55 +0500

    Changes

--- server.js
+++ server.js
@@ -18,14 +18,9 @@ const categoryRoutes = require("./Routes/categoryRoutes");
 
 // Middlewares
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // Allow all origins (for testing) 
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200); // Handle preflight requests
-    }
-
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 app.use(express.json({ limit: "50mb" }));
