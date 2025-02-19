927cb4f9938bc35de50da66d98b86713f1f55abb
Muhammad Shayan <shayanarif666@gmail.com>
2025-02-19 16:01:14 +0500

    Update Controller

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -38,27 +38,24 @@ const addPortfolio = async (req, res) => {
     try {
         const { title, description, category } = req.body; // Getting text fields
         const file = req.file ? req.file.filename : null; // Getting uploaded file
-        console.log("Received Portfolio Request:", req.body); // Debugging
-        console.log("File:", req.file); // Debugging
-        // console.log("Received Data:", title, description, category, file); // Debugging
+        
+        if (!file) {
+            return res.status(400).json({ error: "File is required" });
+        }
 
-        // if (!file) {
-        //     return res.status(400).json({ error: "File is required" });
-        // }
+        // Convert category string into ObjectId
+        const categoryExists = await Category.findOne({ name: category });
+        if (!categoryExists) {
+            return res.status(400).json({ error: "Invalid category selected" });
+        }
 
-        // // Convert category string into ObjectId
-        // const categoryExists = await Category.findOne({ name: category });
-        // if (!categoryExists) {
-        //     return res.status(400).json({ error: "Invalid category selected" });
-        // }
-
-        // const newPortfolio = Portfolio.create({
-        //     src: file,
-        //     title,
-        //     description,
-        //     category: categoryExists._id // Store ObjectId instead of string
-        // });
-        // res.status(201).json(newPortfolio);
+        const newPortfolio = Portfolio.create({
+            src: file,
+            title,
+            description,
+            category: categoryExists._id // Store ObjectId instead of string
+        });
+        res.status(201).json(newPortfolio);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
